bb21eb6cbd87b46c0180255fe322437a3fa6b04f
themanprince <princeadigwe29@gmail.com>
2024-09-23 15:47:42 +0000

    acceptable baseline

--- private/server.js
+++ private/server.js
@@ -2,12 +2,19 @@ const {Server} = require("http");
 const {exists, readFile, lstat} = require("fs");;
 const path = require("path");
 
+const reqGroupDelimeter = "-".repeat(10); //this is for separating / grouping requests made at the same time
+let timeout = null;
+const timeBound = 500;
+function debounceDelimeterPrinting() {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => console.log(reqGroupDelimeter), timeBound);
+}
+
+
 const theServer = new Server(async (req, res) => {
 	
 	let requestedFilePath = (path.dirname(__filename) + "/.." + req.url).replace(/\%\2\0/g, " ");
 	
-	
-	console.log(req.method, "\"" + req.url + "\"");
 	
 	const fileExistsAndIsNotDirectory = new Promise(resolve => {
 	    
@@ -27,6 +34,10 @@ const theServer = new Server(async (req, res) => {
 	
 	
 	if(("GET" == req.method) && (await fileExistsAndIsNotDirectory)) {
+	    
+	    console.log(req.method, "\"" + req.url + "\"");
+    	debounceDelimeterPrinting();
+    	
 		const fileContents = new Promise((resolve, reject) => {
 			readFile(requestedFilePath, (err, result) => {
 				if(err)	reject(err);
@@ -37,18 +48,18 @@ const theServer = new Server(async (req, res) => {
 		
 		let contents = await fileContents;
 		if(path.extname(requestedFilePath).match(/.?html?/)) {
-		let noOfDirsRelativeToAdure  = req.url.split("/").length - 2; //I needed to obtain this variable because when this html file will send a req for the script later...
+		let noOfDirsRelativeToEruda  = req.url.split("/").length - 2; //I needed to obtain this variable because when this html file will send a req for the script later...
 		//e go do like say d kini dy with am
 		//so I dy use this one go back
-		contents += '<script src="' + '../'.repeat(noOfDirsRelativeToAdure)+'/private/adure.js"></script>\n<script>eruda.init();</script>';
+		contents += '<script src="' + '../'.repeat(noOfDirsRelativeToEruda)+'/private/eruda.js"></script>\n<script>eruda.init();</script>';
 		
 		}
 		
 		res.end(contents);
 	} else {
 		res.setHeader("status", 401);
-		res.end("Error!\n1. make sure the file exists\n2. make sure that it is not a folder.\n3. make sure it is a GET method\n4. contact developer otherwise");
+		res.end("Error!\n\n1. make sure the file requested for exists\n2. make sure you did not request for a folder instead of a file.\n3. contact the developer");
 	}
 });
 
-theServer.listen(8000, () => console.log("The Server is Active\nThe Web Address is localhost:8000"));
+theServer.listen(8000, () => console.log("The Server is Active\nThe Web Address is localhost:8000\n"));
