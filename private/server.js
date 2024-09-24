38cf4d20471e6882258f73fb747e0663470a493d
themanprince <princeadigwe29@gmail.com>
2024-09-24 01:39:02 +0000

    before new folder arrangement came

--- private/server.js
+++ private/server.js
@@ -2,14 +2,38 @@ const {Server} = require("http");
 const {exists, readFile, lstat} = require("fs");;
 const path = require("path");
 
-const reqGroupDelimeter = "-".repeat(10); //this is for separating / grouping requests made at the same time
-let timeout = null;
-const timeBound = 500;
+const reqGroupDelimeter = "-".repeat(10); //this delimeter is for separating / grouping requests made at the same time
+let debounceTimeout = null;
+const debounceTime = 500;
 function debounceDelimeterPrinting() {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => console.log(reqGroupDelimeter), timeBound);
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => console.log(reqGroupDelimeter), debounceTime);
 }
 
+const log = function(wasFound, url) {
+    
+    const date = new Date();
+    let hrs, mins, secs, meridian, timeStr;
+    const is12HrStyle = true;
+    
+    hrs = (date.getHours() + 1) % 24; // '+1' because nodejs getHours() returns a number that is one-less than the actual no of hours, in 24-hrs time style
+    if ((hrs >= 0) && (hrs <= 11))
+        meridian = "AM";
+    else
+        meridian = "PM";
+    
+    if((is12HrStyle) && (hrs > 12))
+        hrs = hrs - 12;
+    
+    mins = date.getMinutes();
+    secs = date.getSeconds();
+    timeStr = `${hrs}:${mins}:${secs} ${meridian}`;
+    foundStr = (wasFound) ? "OK" : "404";
+    reqStr = `${url} - ${foundStr}`;
+         	
+    const strToLog = `[${timeStr}] ${reqStr}`;
+    console.log(strToLog);
+}
 
 const theServer = new Server(async (req, res) => {
 	
@@ -33,33 +57,40 @@ const theServer = new Server(async (req, res) => {
 	});
 	
 	
-	if(("GET" == req.method) && (await fileExistsAndIsNotDirectory)) {
+	if("GET" == req.method) {
 	    
-	    console.log(req.method, "\"" + req.url + "\"");
-    	debounceDelimeterPrinting();
-    	
-		const fileContents = new Promise((resolve, reject) => {
-			readFile(requestedFilePath, (err, result) => {
-				if(err)	reject(err);
+	   	if(await fileExistsAndIsNotDirectory) {
+	        log(true, req.url);
+             	
+    		const fileContents = new Promise((resolve, reject) => {
+    			readFile(requestedFilePath, (err, result) => {
+    				if(err)	reject(err);
 				
-				resolve(result);
-			})
-		});
+    				resolve(result);
+    			})
+	    	});
 		
-		let contents = await fileContents;
-		if(path.extname(requestedFilePath).match(/.?html?/)) {
-		let noOfDirsRelativeToEruda  = req.url.split("/").length - 2; //I needed to obtain this variable because when this html file will send a req for the script later...
-		//e go do like say d kini dy with am
-		//so I dy use this one go back
-		contents += '<script src="' + '../'.repeat(noOfDirsRelativeToEruda)+'/private/eruda.js"></script>\n<script>eruda.init();</script>';
-		
-		}
-		
-		res.end(contents);
-	} else {
-		res.setHeader("status", 401);
-		res.end("Error!\n\n1. make sure the file requested for exists\n2. make sure you did not request for a folder instead of a file.\n3. contact the developer");
+    		let contents = await fileContents;
+    		if(path.extname(requestedFilePath).match(/.?html?/)) {
+    		    let noOfDirsRelativeToAdure  = req.url.split("/").length - 2; //I needed to obtain this variable because when this html file will send a req for the script later...
+                //to understand previous line, consider the result of "/home".split("/")
+                //you get ["", "home"] as the result... this has a length of 2.... Thus, the value of above variable will be 0
+                
+                //check it for other url such as "/proejct1/style.css"... you'll see that the next line of code is to take you to the root of ProjectFolder
+    	    	contents += '<script src="' + '../'.repeat(noOfDirsRelativeToAdure)+'/private/adure.js"></script>\n<script>eruda.init();</script>';
+    		
+    		}
+	    	
+    		res.end(contents);
+    	} else {
+    	    log(false, req.url);
+    		res.setHeader("status", 401);
+    		res.end("Error!\n\n1. make sure the file requested for exists\n2. make sure you did not request for a folder instead of a file.\n3. contact the developer");
+    	}
 	}
+      
+    debounceDelimeterPrinting();
+
 });
 
 theServer.listen(8000, () => console.log("The Server is Active\nThe Web Address is localhost:8000\n"));
